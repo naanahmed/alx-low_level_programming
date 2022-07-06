0d668ebba53553b0d3e03b83043eb2f435d63124
Naan Ahmed <naanahmed.na@gmail.com>
2022-07-06 12:20:39 -0700

    edit 3 for Q1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,13 @@
-nclude "function_pointers.h"
+include "function_pointers.h"
 
 /**
- *
- *  *print_name - prints name
- *
- *   *@name: string
- *
- *    *@f: functions
- *
- *     *Return: void
- *
- *      */
-
-void print_name(char *name, void (*f)(char *))
-
+ * print_name - prints name
+ * @name: string
+ * @f: functions
+ * Return: void
+ */
+ void print_name(char *name, void (*f)(char *))
 {
-
 	if (f != NULL && name != NULL)
-
 		f(name);
-
 }
